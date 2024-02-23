3cb93e35fcc4cb832abc179fd24c88da1ec71090
Mykel68 <olowookeremicheal9568@gmail.com>
2024-02-23 21:49:51 +0100

    live status updated on listeners count

--- server/routes/listenersCountRoutes.js
+++ server/routes/listenersCountRoutes.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const ListenersCount = require('../models/ListenersCount'); 
+const ListenersCount = require('../models/ListenersCount');
 
 router.get('/listenersCount', async (req, res) => {
   try {
     // Fetch the document with the highest count from the database
     const highestEntry = await ListenersCount.findOne().sort({ count: -1 }).limit(1);
 
+    // Determine if the event is live based on the current date and time
+    let isLive = false;
+    if (highestEntry) {
+      const currentDate = new Date();
+      const startTime = new Date(highestEntry.startTime);
+      const endTime = new Date(highestEntry.endTime);
+
+      isLive = currentDate >= startTime && currentDate <= endTime;
+    }
+
     // Respond with the highest count, event, and live status as JSON
     res.json({
       count: highestEntry ? highestEntry.count : 0,
       event: highestEntry ? highestEntry.event : 'No event',
-      isLive: highestEntry ? true : false,
+      isLive,
     });
   } catch (error) {
     console.error('Error fetching listeners count:', error.message);
